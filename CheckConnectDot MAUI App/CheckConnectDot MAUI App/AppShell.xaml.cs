cb8552b0ffcc17afa1c3ce08e7886436b1358774
GMan2605 <gavinblackie2026@gmail.com>
2025-03-18 13:50:43 -0400

    Added the MAUI template

---
+++ CheckConnectDot MAUI App/CheckConnectDot MAUI App/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CheckConnectDot_MAUI_App
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
