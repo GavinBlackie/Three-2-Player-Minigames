a9f5933dcad6570a0050b2461f30c46e4d9e89f6
GMan2605 <gavinblackie2026@gmail.com>
2025-03-18 19:43:00 -0400

    Added folders for each game's logic and page files to be located in

--- CheckConnectDot MAUI App/CheckConnectDot MAUI App/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/CheckConnectDot MAUI App.AssemblyInfo.cs
+++ CheckConnectDot MAUI App/CheckConnectDot MAUI App/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/CheckConnectDot MAUI App.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CheckConnectDot MAUI App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+d66fe065219931606c8cd76134dea38ed8a4069f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+c9291c7244955ba96fb9a8ae2a3fabbab92e8fff")]
 [assembly: System.Reflection.AssemblyProductAttribute("CheckConnectDot MAUI App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CheckConnectDot MAUI App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
