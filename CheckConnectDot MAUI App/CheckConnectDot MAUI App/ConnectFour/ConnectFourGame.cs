b8b2a1b832d2b3f486f8716735f012e0c5010aa6
Artem Kotliar <kotliar@sheridancollege.ca>
2025-04-03 14:41:11 -0400

    Implemented a basic Connect4Page, using temporary assets. Implemented basic setup for the project (methods are to be implemented).

--- CheckConnectDot MAUI App/CheckConnectDot MAUI App/ConnectFour/ConnectFourGame.cs
+++ CheckConnectDot MAUI App/CheckConnectDot MAUI App/ConnectFour/ConnectFourGame.cs
@@ -1,16 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CheckConnectDot_MAUI_App.ConnectFour
+﻿namespace CheckConnectDot_MAUI_App.ConnectFour
 {
     public class ConnectFourGame : Game
     {
+        private Player _currentPlayer;
+        private Player[] _players;
+        private int[,] _board;
+        private int _disksLeft1;
+        private int _disksLeft2;
+
+        private const string RED_DISK_DIR = "player1_disk.png";
+        private const string BLUE_DISK_DIR = "player2_disk.png";
+
+        
         public ConnectFourGame(ref (Player, Player) playersTuple) : base(ref playersTuple)
         {
+            _players = new Player[] { playersTuple.Item1, playersTuple.Item2 };
+            _currentPlayer = _players[0]; // Player 1 starts
+            _board = new int[7, 6]; // 7 columns, 6 rows
+            _disksLeft1 = 21;
+            _disksLeft2 = 21;
+        }
 
+        public Player CurrentPlayer { get { return _currentPlayer; } }
+        public int DisksLeft1 { get { return _disksLeft1; } }
+        public int DisksLeft2 { get { return _disksLeft2; } }
+
+        public void DropDisk(int column)
+        {
+            
+        }
+
+        public void ResetGame()
+        {
+            // Reset to initial state
+        }
+
+        protected bool IsGameOver()
+        {
+            // Check for 4 in a row, column, or diagonal
+            return false;
         }
     }
 }
--- CheckConnectDot MAUI App/CheckConnectDot MAUI App/ConnectFour/ConnectFourPage.xaml.cs
+++ CheckConnectDot MAUI App/CheckConnectDot MAUI App/ConnectFour/ConnectFourPage.xaml.cs
@@ -4,12 +4,125 @@ namespace CheckConnectDot_MAUI_App;
 
 public partial class ConnectFourPage : ContentPage
 {
-	private ConnectFourGame _connectFourGame;
+    private ConnectFourGame _connectFourGame;
+    private Image[,] _boardImages;
 
-	public ConnectFourPage(ConnectFourGame connectFourGame)
-	{
-		_connectFourGame = connectFourGame; // Contain the given singleton of a connectFourGame
-        
+    public ConnectFourPage(ConnectFourGame connectFourGame)
+    {
+        _connectFourGame = connectFourGame;
         InitializeComponent();
-	}
+        InitializeBoardReferences();
+        SetParams();
+        UpdateGameDisplay();
+    }
+
+    private void InitializeBoardReferences()
+    {
+        _boardImages = new Image[7, 6];
+        
+        // Column 0
+        _boardImages[0, 0] = Cell_0_0;
+        _boardImages[0, 1] = Cell_0_1;
+        _boardImages[0, 2] = Cell_0_2;
+        _boardImages[0, 3] = Cell_0_3;
+        _boardImages[0, 4] = Cell_0_4;
+        _boardImages[0, 5] = Cell_0_5;
+        
+        // Column 1
+        _boardImages[1, 0] = Cell_1_0;
+        _boardImages[1, 1] = Cell_1_1;
+        _boardImages[1, 2] = Cell_1_2;
+        _boardImages[1, 3] = Cell_1_3;
+        _boardImages[1, 4] = Cell_1_4;
+        _boardImages[1, 5] = Cell_1_5;
+        
+        // Column 2
+        _boardImages[2, 0] = Cell_2_0;
+        _boardImages[2, 1] = Cell_2_1;
+        _boardImages[2, 2] = Cell_2_2;
+        _boardImages[2, 3] = Cell_2_3;
+        _boardImages[2, 4] = Cell_2_4;
+        _boardImages[2, 5] = Cell_2_5;
+        
+        // Column 3
+        _boardImages[3, 0] = Cell_3_0;
+        _boardImages[3, 1] = Cell_3_1;
+        _boardImages[3, 2] = Cell_3_2;
+        _boardImages[3, 3] = Cell_3_3;
+        _boardImages[3, 4] = Cell_3_4;
+        _boardImages[3, 5] = Cell_3_5;
+        
+        // Column 4
+        _boardImages[4, 0] = Cell_4_0;
+        _boardImages[4, 1] = Cell_4_1;
+        _boardImages[4, 2] = Cell_4_2;
+        _boardImages[4, 3] = Cell_4_3;
+        _boardImages[4, 4] = Cell_4_4;
+        _boardImages[4, 5] = Cell_4_5;
+        
+        // Column 5
+        _boardImages[5, 0] = Cell_5_0;
+        _boardImages[5, 1] = Cell_5_1;
+        _boardImages[5, 2] = Cell_5_2;
+        _boardImages[5, 3] = Cell_5_3;
+        _boardImages[5, 4] = Cell_5_4;
+        _boardImages[5, 5] = Cell_5_5;
+        
+        // Column 6
+        _boardImages[6, 0] = Cell_6_0;
+        _boardImages[6, 1] = Cell_6_1;
+        _boardImages[6, 2] = Cell_6_2;
+        _boardImages[6, 3] = Cell_6_3;
+        _boardImages[6, 4] = Cell_6_4;
+        _boardImages[6, 5] = Cell_6_5;
+    }
+
+    protected void OnCol(object sender, EventArgs e)
+    {
+        
+    }
+
+    private void UpdateCellImage(int col, int row, string imageSource)
+    {
+        
+    }
+
+    private void UpdateTurnDisplay()
+    {
+        
+    }
+
+    private void UpdateDiskCounts(int diskCount1, int diskCount2)
+    {
+        
+    }
+
+    private void UpdateGameDisplay()
+    {
+        
+    }
+
+    protected void SetParams()
+    {
+        Col1.CommandParameter = 1;
+        Col1.Clicked += OnCol;
+        
+        Col2.CommandParameter = 2;
+        Col2.Clicked += OnCol;
+        
+        Col3.CommandParameter = 3;
+        Col3.Clicked += OnCol;
+        
+        Col4.CommandParameter = 4;
+        Col4.Clicked += OnCol;
+        
+        Col5.CommandParameter = 5;
+        Col5.Clicked += OnCol;
+        
+        Col6.CommandParameter = 6;
+        Col6.Clicked += OnCol;
+        
+        Col7.CommandParameter = 7;
+        Col7.Clicked += OnCol;
+    }
 }
