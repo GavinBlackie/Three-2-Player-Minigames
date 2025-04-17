edba38c93d0fa7b341b10e740b31dddd82938cad
Joseph Thomas <thoma118@sheridancollege.ca>
2025-04-17 14:23:51 -0400

    Created default xaml setup for visualization of dots and boxes, and can click to get a buttons coordinates

--- CheckConnectDot MAUI App/CheckConnectDot MAUI App/DotsBoxes/DotsBoxesPage.xaml.cs
+++ CheckConnectDot MAUI App/CheckConnectDot MAUI App/DotsBoxes/DotsBoxesPage.xaml.cs
@@ -6,10 +6,27 @@ public partial class DotsBoxesPage : ContentPage
 {
 	private DotsBoxesGame _dotsBoxesGame;
 
-	public DotsBoxesPage(DotsBoxesGame dotsBoxesGame)
+    private Tuple<int, int>? _dotsCoords1;
+    private Tuple<int, int>? _dotsCoords2;
+
+    public DotsBoxesPage(DotsBoxesGame dotsBoxesGame)
 	{
 		_dotsBoxesGame = dotsBoxesGame; // Contain the given singleton of a dotsBoxesGame 
-        
+        _dotsCoords1 = null;
+        _dotsCoords2 = null;
+
         InitializeComponent();
 	}
+
+	public async void OnDotClicked(object sender, EventArgs e)
+	{
+        if (sender is View view)
+        {
+            int row = Grid.GetRow(view);
+            int column = Grid.GetColumn(view);
+
+            await DisplayAlert("Button Clicked", $"Box clicked at row {row}, column {column}", "Ok");
+
+        }
+    }
 }
